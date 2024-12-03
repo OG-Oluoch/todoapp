7e5e02fb6353ec03f1afd21f2397ab653f90bf22
OG-Oluoch <makopiyo45@gmail.com>
2024-12-03 07:52:24 +0300

    fixed repo url

--- src/main/java/com/app/todoapp/services/TodoService.java
+++ src/main/java/com/app/todoapp/services/TodoService.java
@@ -37,5 +37,6 @@ public class TodoService {
                 .orElseThrow(()->new IllegalArgumentException("invalid task id"));
                 task.setCompleted(!task.isCompleted());
                 taskRepository.save(task);
+
     }
 }
